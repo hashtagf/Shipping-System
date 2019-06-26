8371cfff37b98dced10449d5a5a76ec39452fbe6
Moshi <moszie248@hotmail.com>
2019-06-26 18:53:37 +0700

    ProductForm1

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -10,8 +10,10 @@
         <h4>เพิ่มข้อมูลสินค้า</h4>
       </b-col>
     </b-row>
+
     <b-form @submit.prevent="onSubmit">
       <b-row>
+
         <b-col cols="6">
           <b-form-group id="name" label="ชื่อสินค้า" label-for="name">
             <b-form-input
@@ -23,49 +25,93 @@
             ></b-form-input>
           </b-form-group>
         </b-col>
+
         <b-col cols="6">
-          <b-form-group id="name" label="ชื่อสินค้า" label-for="name">
+          <b-form-group id="price" label="ราคาขาย" label-for="price">
             <b-form-input
-              id="name"
+              id="price"
               v-model="form.price"
               type="text"
               required
-              placeholder="ชือสินค้า"
+              placeholder="ราคาขาย"
             ></b-form-input>
           </b-form-group>
         </b-col>
+
         <b-col cols="6">
-          <b-form-group id="name" label="ชื่อสินค้า" label-for="name">
+          <b-form-group id="cost" label="ราคาต้นทุน" label-for="cost">
             <b-form-input
-              id="name"
-              v-model="form.name"
+              id="cost"
+              v-model="form.cost"
               type="text"
               required
-              placeholder="ชือสินค้า"
+              placeholder="ราคาทุน"
             ></b-form-input>
           </b-form-group>
         </b-col>
+
         <b-col cols="6">
-          <b-form-group id="name" label="ชื่อสินค้า" label-for="name">
+          <b-form-group id="type" label="ประเภทสินค้า" label-for="type">
             <b-form-input
-              id="name"
-              v-model="form.name"
+              id="type"
+              v-model="form.type"
               type="text"
               required
-              placeholder="ชือสินค้า"
+              placeholder="ประเภทสินค้า"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="6">
+          <b-form-group id="export" label="ร้านส่งออก" label-for="export">
+            <b-form-input
+              id="export"
+              v-model="form.export"
+              type="text"
+              required
+              placeholder="ร้านส่งออก"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="6">
+          <b-form-group id="sign" label="เซ็นรับ" label-for="sign">
+            <b-form-input
+              id="sign"
+              v-model="form.sign"
+              type="text"
+              required
+              placeholder="เซ็นรับ"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="6">
+          <b-form-group id="import" label="เข้าโกดังไทย" label-for="import">
+            <b-form-input
+              id="import"
+              v-model="form.import"
+              type="text"
+              required
+              placeholder="เข้าโกดังไทย"
             ></b-form-input>
           </b-form-group>
         </b-col>
         <b-col cols="6"></b-col>
-        <b-col cols="6"></b-col>
+
+
         <b-col cols="6">
           <b-button type="submit" variant="primary">เพิ่มสินค้า</b-button>
         </b-col>
         <b-col cols="6">
           <b-button type="reset" variant="danger">ยกเลิก</b-button>
         </b-col>
+
+        
+
       </b-row>
     </b-form>
+
   </b-container>
 </template>
 
@@ -85,7 +131,12 @@ export default {
       console.log(this.form);
       productFirestore.add({
         name: this.form.name,
-        price: "wdsdasad"
+        price: this.form.price,
+        cost: this.form.cost,
+        type: this.form.type,
+        export: this.form.export,
+        sign: this.form.sign,
+        import: this.form.import
       });
     }
   },
