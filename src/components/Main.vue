330e801deb5a3e08223d601a7f86b488dc050b18
Moshi <moszie248@hotmail.com>
2019-07-01 16:40:46 +0700

    main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,53 @@
 <template>
-  <div></div>
+  <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">รายการ</th>
+            <th scope="col">วันที่</th>
+            <th scope="col">ลูกค้า</th>
+            <th scope="col">รายการสินค้า</th>
+            <th scope="col">คุณสมบัติ</th>
+            <th scope="col">ราคาขาย</th>
+            <th scope="col">ราคาต้นทุน</th>
+            <th scope="col">รวม</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(val) in billing" :key="val.id">
+            <td>{{val.data}}</td>
+    
+          </tr>
+        </tbody>
+      </table>
+
+  </div>
+  
 </template>
 
 <script>
-export default {};
+import firebase from "firebase";
+var billingFirestore = firebase.firestore().collection("Billings");
+export default {
+  name: "Billing",
+  data() {
+    return {
+      billing: [],
+ 
+    };
+  },
+  mounted() {
+    billingFirestore.onSnapshot(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        this.billing.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      });
+      console.log(this.billing);
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
