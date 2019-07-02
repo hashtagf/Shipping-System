acfacef3ac0b6ba8635fe3c932affbf955306622
flamezaxaou10 <flamezaxaou1@gmail.com>
2019-07-02 12:55:49 +0700

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="Header mb-3">
     <b-navbar toggleable="lg" type="dark" variant="dark ">
-      <b-navbar-brand href="#">Shipping-System</b-navbar-brand>
+      <b-navbar-brand href="/">Shipping-System</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/">หน้าหลัก</b-nav-item>
+          <b-nav-item to="/">บิลลูกค้า</b-nav-item>
           <b-nav-item to="/Billing">จัดการบิล</b-nav-item>
           <b-nav-item-dropdown text="จัดการข้อมูล" left>
             <b-dropdown-item to="/Product">สินค้า</b-dropdown-item>
             <b-dropdown-item to="/Customer">ลูกค้า</b-dropdown-item>
           </b-nav-item-dropdown>
+          <b-nav-item to="/Report">รายงานสรุป</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,16 +12,14 @@
           <th scope="col">รายการสินค้า</th>
           <th scope="col">คุณสมบัติ</th>
           <th scope="col">จำนวน</th>
-          <th scope="col">ราคาขาย</th>
-          <th scope="col">ราคาต้นทุน</th>
-          <th scope="col">รวมราคาขาย</th>
-          <th scope="col">รวมราคาต้นทุน</th>
+          <th scope="col">ราคา</th>
+          <th scope="col">รวมราคา</th>
           <th scope="col">จัดการบิล</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(val, index) in billing" :key="val.id">
-          <th scope="row">{{index+1}}</th>
+          <th scope="row">{{index + 1}}</th>
           <td>{{val.data.timestamp | moment("DD/MM/Y")}}</td>
           <td>
             <customer-name :idCustomer="val.data.customer" />
@@ -56,28 +54,13 @@
               :key="index"
             >{{product.product.price}}</div>
           </td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index)  in val.data.billing"
-              :key="index"
-            >{{product.product.cost}}</div>
-          </td>
           <th>
             <div
               class="border-bottom"
               v-for="(product,index) in val.data.billing"
               :key="index"
-            >{{product.product.price* product.count}}</div>
+            >{{product.product.price * product.count}}</div>
             <b class="text-primary">{{val.data.total.price}}</b>
-          </th>
-          <th>
-            <div
-              class="border-bottom"
-              v-for="(product,index) in val.data.billing"
-              :key="index"
-            >{{product.product.cost* product.count}}</div>
-            <b class="text-primary">{{val.data.total.cost}}</b>
           </th>
           <td>
             <b-button type="submit" size="sm" variant="primary" class="my-auto">จัดการค่าขนส่ง</b-button>
