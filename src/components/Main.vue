a22085c9acd44674e46e4e8c8d27a241380bbaaf
flamezaxaou10 <flamezaxaou1@gmail.com>
2019-07-03 12:40:11 +0700

    table responsive

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,83 +8,85 @@
         <vs-button color="primary" type="filled" icon="add_circle">เปิดบิล</vs-button>
       </router-link>
     </div>
-    <table class="table col-10 border table-hover table-bordered">
-      <thead class="thead-light">
-        <tr>
-          <th scope="col">รายการ</th>
-          <th scope="col">วันที่</th>
-          <th scope="col">ลูกค้า</th>
-          <th scope="col">รายการสินค้า</th>
-          <th scope="col">คุณสมบัติ</th>
-          <th scope="col">จำนวน</th>
-          <th scope="col">ราคา</th>
-          <th scope="col">รวมราคา</th>
-          <th scope="col">รวมราคา (THB)</th>
-          <th scope="col">จัดการบิล</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(val, index) in billing" :key="val.id">
-          <th scope="row">{{index + 1}}</th>
-          <td>{{val.data.timestamp | moment("DD/MM/Y")}}</td>
-          <td v-if="customer[index]">{{customer[index].nickname}}</td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index) in val.data.billing"
-              :key="index"
-            >{{product.product.name}}</div>
-          </td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index)  in val.data.billing"
-              :key="index"
-            >{{product.properties}}</div>
-            <b class="text-primary">{{"รวม"}}</b>
-          </td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index)  in val.data.billing"
-              :key="index"
-            >{{product.count}}</div>
-            <b class="text-primary">{{val.data.total.count}}</b>
-          </td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index)  in val.data.billing"
-              :key="index"
-            >{{product.product.price}}</div>
-          </td>
-          <td>
-            <div
-              class="border-bottom"
-              v-for="(product,index) in val.data.billing"
-              :key="index"
-            >{{product.product.price * product.count}}</div>
-            <b
-              class="text-primary"
-            >{{new Intl.NumberFormat({ style: 'currency'}).format(val.data.total.price)}}</b>
-          </td>
-          <td>
-            <b
-              class="text-success"
-            >{{new Intl.NumberFormat({ style: 'currency'}).format(val.data.total.price * val.data.rateTHBprice)}}</b>
-          </td>
-          <td>
-            <b-button
-              type="submit"
-              size="sm"
-              variant="primary"
-              class="my-auto"
-              :to="'/Shipping/' + val.id"
-            >จัดการค่าขนส่ง</b-button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="col-10 table-responsive">
+      <table class="table border table-hover table-bordered">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">รายการ</th>
+            <th scope="col">วันที่</th>
+            <th scope="col">ลูกค้า</th>
+            <th scope="col">รายการสินค้า</th>
+            <th scope="col">คุณสมบัติ</th>
+            <th scope="col">จำนวน</th>
+            <th scope="col">ราคา</th>
+            <th scope="col">รวมราคา</th>
+            <th scope="col">รวมราคา (THB)</th>
+            <th scope="col">จัดการบิล</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(val, index) in billing" :key="val.id">
+            <th scope="row">{{index + 1}}</th>
+            <td>{{val.data.timestamp | moment("DD/MM/Y")}}</td>
+            <td v-if="customer[index]">{{customer[index].nickname}}</td>
+            <td>
+              <div
+                class="border-bottom"
+                v-for="(product,index) in val.data.billing"
+                :key="index"
+              >{{product.product.name}}</div>
+            </td>
+            <td>
+              <div
+                class="border-bottom"
+                v-for="(product,index)  in val.data.billing"
+                :key="index"
+              >{{product.properties}}</div>
+              <b class="text-primary">{{"รวม"}}</b>
+            </td>
+            <td>
+              <div
+                class="border-bottom"
+                v-for="(product,index)  in val.data.billing"
+                :key="index"
+              >{{product.count}}</div>
+              <b class="text-primary">{{val.data.total.count}}</b>
+            </td>
+            <td>
+              <div
+                class="border-bottom"
+                v-for="(product,index)  in val.data.billing"
+                :key="index"
+              >{{product.product.price}}</div>
+            </td>
+            <td>
+              <div
+                class="border-bottom"
+                v-for="(product,index) in val.data.billing"
+                :key="index"
+              >{{product.product.price * product.count}}</div>
+              <b
+                class="text-primary"
+              >{{new Intl.NumberFormat({ style: 'currency'}).format(val.data.total.price)}}</b>
+            </td>
+            <td>
+              <b
+                class="text-success"
+              >{{new Intl.NumberFormat({ style: 'currency'}).format(val.data.total.price * val.data.rateTHBprice)}}</b>
+            </td>
+            <td>
+              <b-button
+                type="submit"
+                size="sm"
+                variant="primary"
+                class="my-auto"
+                :to="'/Shipping/' + val.id"
+              >จัดการค่าขนส่ง</b-button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -124,7 +126,6 @@ export default {
       });
       this.$vs.loading.close();
     });
-    console.log(this.customer);
   }
 };
 </script>
